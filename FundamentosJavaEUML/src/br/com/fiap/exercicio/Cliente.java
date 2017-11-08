b4d60270109499c647a5334833f71657ee447934
logonrm <logonrm@L2704MICRO03.fiap.com.br>
2017-11-07 22:45:59 -0200

    aula 07/11/2017

--- FundamentosJavaEUML/src/br/com/fiap/exercicio/Cliente.java
+++ FundamentosJavaEUML/src/br/com/fiap/exercicio/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.fiap.exercicio;
 
+
 import java.time.LocalDate;
 
 public class Cliente{
--- FundamentosJavaEUML/src/br/com/fiap/exercicio/TesteStreamsLambda.java
+++ FundamentosJavaEUML/src/br/com/fiap/exercicio/TesteStreamsLambda.java
@@ -1,5 +1,6 @@
 package br.com.fiap.exercicio;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
